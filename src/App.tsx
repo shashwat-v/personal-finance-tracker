57f51e1b7469e08b42b023c2b7cd943cda0f6da2
harshit-dev-ui <65815998+harshit-dev-ui@users.noreply.github.com>
2024-06-26 11:13:32 +0530

    create fiile/folder structure for frontend

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-7xl bg-gray-600 h-screen w-full">
+        Initialising Project
+      </h1>
+    </>
+  );
+}
+
+export default App;
